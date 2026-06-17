7878a73c76500c3a46299b15c8138028ae03b2bf
Justice3984 <90700124+Justice3984@users.noreply.github.com>
2026-06-17 12:38:59 +0100

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -18,7 +18,7 @@ export const protect = (req, res, next) => {
 
 // ✅ Allow only admins
 export const isAdmin = (req, res, next) => {
-  if (!req.user || req.user.role !== "admin") {
+  if (!req.user || !["admin", "superadmin"].includes(req.user.role)) {
     return res.status(403).json({ message: "Forbidden: Admins only" });
   }
   next();
